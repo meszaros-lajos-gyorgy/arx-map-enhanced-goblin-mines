58cb76c541e39c371dce91f5adfe0b1a6258c82c
Lajos Meszaros <m_lajos@hotmail.com>
2024-06-02 15:46:08 +0200

    simplify code with latest version of arx-level-generator

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { $, ArxMap, Entity, EntityModel, Polygon, Rotation, Settings, Vector3, Vertex } from 'arx-level-generator'
 import { useDelay } from 'arx-level-generator/scripting/hooks'
-import { Label, Material } from 'arx-level-generator/scripting/properties'
+import { Collision, Label, Material, Shadow } from 'arx-level-generator/scripting/properties'
+import { circleOfVectors } from 'arx-level-generator/utils'
 import { Box3, MathUtils } from 'three'
 
 // ------------------------------------------------
@@ -23,65 +24,51 @@ const spawnMarkerIdx = level15.entities.findIndex((entity) => entity === spawnMa
 level15.entities.splice(spawnMarkerIdx, 1)
 
 // ------------------------------------------------
-// adjusting stuff
+// making the water wheel rotate
+
+// 1) extracting the polygons from the mesh
 
 const wheelBox = new Box3(new Vector3(7940, 1075, 8825), new Vector3(8050, 1355, 9120))
-const wheelSelection = $(level15.polygons)
+const axleBox = new Box3(
+  new Vector3(7950 - 27, 1075 + 100, 8825 + 100),
+  new Vector3(8045 + 101, 1355 - 100, 9120 - 100),
+)
+
+const waterWheelPolygons = $(level15.polygons)
   .selectWithinBox(wheelBox)
   .selectBy((polygon) => {
     if (polygon.texture === undefined) {
       return true
     }
+
     const { filename } = polygon.texture
     return !(filename.includes('_ground') || filename.includes('_piece'))
   })
+  .delete()
 
-const waterWheelPolygons = wheelSelection.copy().get()
-
-wheelSelection.delete()
-
-const axleBox = new Box3(
-  new Vector3(7950 - 27, 1075 + 100, 8825 + 100),
-  new Vector3(8045 + 101, 1355 - 100, 9120 - 100),
-)
-const axleSelection = $(level15.polygons)
+const axlePolygons = $(level15.polygons)
   .clearSelection()
   .selectWithinBox(axleBox)
   .selectBy((p) => {
     return !waterWheelPolygons.some((wp) => wp.equals(p))
   })
+  .delete()
 
-waterWheelPolygons.push(...axleSelection.copy().get())
+waterWheelPolygons.push(...axlePolygons)
 
-axleSelection.delete()
+// 2) adding a center point to the polygons
 
-// ----------------
-
-const sizeOfWaterWheel = new Box3()
-
-waterWheelPolygons.forEach((polygon) => {
-  for (let i = 0; i < (polygon.isQuad() ? 4 : 3); i++) {
-    sizeOfWaterWheel.expandByPoint(polygon.vertices[i])
-  }
-})
-
-const { max: wwMax, min: wwMin } = sizeOfWaterWheel
-const halfX = (wwMax.x - wwMin.x) / 2
-const halfY = (wwMax.y - wwMin.y) / 2
-const halfZ = (wwMax.z - wwMin.z) / 2
-const centerOfWaterWheel = new Vertex(wwMin.x + halfX, wwMin.y + halfY, wwMin.z + halfZ)
+const centerOfWaterWheel = waterWheelPolygons.getCenter()
+const [a, b, c] = circleOfVectors(centerOfWaterWheel, 1, 3)
 
 const waterWheelCenterPolygon = new Polygon({
-  vertices: [
-    centerOfWaterWheel.clone().add(new Vector3(1, 0, 0)),
-    centerOfWaterWheel.clone().add(new Vector3(1, 0, 1)),
-    centerOfWaterWheel.clone().add(new Vector3(0, 0, 1)),
-    new Vertex(0, 0, 0),
-  ],
+  vertices: [new Vertex(a.x, a.y, a.z), new Vertex(b.x, b.y, b.z), new Vertex(c.x, c.y, c.z), new Vertex(0, 0, 0)],
   isQuad: false,
 })
 
 waterWheelPolygons.unshift(waterWheelCenterPolygon)
+
+// 3) turning the polygons into an entity
 
 const waterWheel = new Entity({
   src: 'fix_inter/water_wheel',
@@ -90,15 +77,22 @@ const waterWheel = new Entity({
     sourcePath: './entities/water_wheel',
     originIdx: 0,
   }),
-  position: new Vector3(8903 - 27 + halfX, 192 - 36 + halfY, 651 - 57 + halfZ),
+  position: new Vector3(
+    8903 - 27 + waterWheelPolygons.getWidth() / 2,
+    192 - 36 + waterWheelPolygons.getHeight() / 2,
+    651 - 57 + waterWheelPolygons.getDepth() / 2,
+  ),
   orientation: new Rotation(0, MathUtils.degToRad(-90), 0),
 })
 waterWheel.withScript()
-waterWheel.script?.properties.push(new Label('water wheel'), Material.wood)
+waterWheel.script?.properties.push(new Label('water wheel'), Material.wood, Collision.off, Shadow.off)
+
+// 4) adding rotation via scripts
+
 waterWheel.script?.on('init', () => {
   const { loop } = useDelay()
   const fps = 30
-  const fullRevolutionTimeInSec = 3
+  const fullRevolutionTimeInSec = 10
   return `${loop(1000 / fps, Infinity)} rotate ${-360 / fps / fullRevolutionTimeInSec} 0 0`
 })
 
